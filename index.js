6b5a5a33170fd3bfa9e012f9bcceebd31f31366f
Vidhun Vinod <vidhun@gmail.com>
2018-01-23 12:59:34 +0530

    init telegram bot

---
+++ index.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = '533091025:AAEYbCc7meRtZN2DJlIYDyZ37BQSAJuGm5c'
+const options = {
+  webHook: {
+    port: 88
+  }
+};
+
+const url = 'https://megaex.io:88';
+const bot = new TelegramBot(TOKEN, options);
+
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'Hello World');
+});
