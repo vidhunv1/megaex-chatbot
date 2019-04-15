1e67985b1e1a1c61638cc1aa732e55374a9d7918
vidhun <vidhunv1@gmail.com>
2019-04-15 23:26:25 +0530

    fix(i18n): wallet -> send bitcoin

--- src/locales/en/walletEN.ts
+++ src/locales/en/walletEN.ts
@@ -17,18 +17,18 @@ export const walletEN = {
 
   'send-cryptocurrency-amount': `*Enter amount*
 
-*Available*: {{ cryptoCurrencyBalance }}
-(Value:       {{ fiatValue }})
-
 Input your amount in  *{{ cryptoCurrencyCode }}* or *{{ fiatCurrencyCode }}*.
 _Example: {{ cryptoCurrencyBalance }}_
+
+*Available*: {{ cryptoCurrencyBalance }}
+(Value:       {{ fiatValue }})
 `,
 
   'send-cryptocurrency-insufficient-balance': `❌ *Insufficient funds*
+  
+You need to fund your {{ cryptoCurrencyCode }} wallet to make this payment.
 
 *Available balance*: {{ cryptoCurrencyBalance}}
-
-Please fund your {{ cryptoCurrencyCode }} wallet to make this payment.
 `,
   'send-cryptocurrency-invalid-amount': `❌ *Invalid amount*
 
@@ -37,17 +37,17 @@ Example: _100 INR or 0.0005 BTC_
 `,
   'confirm-send-cryptocurrency': `*Verify*
 
-Confirm your transaction:
+Check and confirm your transaction:
 
 *Amount: {{ cryptoCurrencyAmount }}*
 (Value:   {{ fiatValue }})
 `,
   'confirm-send-cryptocurrency-button': '✅ Confirm',
-  'show-payment-link': `*Link generated*
+  'show-payment-link': `✅ *Link generated*
 
 {{ paymentLink }}
 (_Share this link privately to send the funds. Anyone with access to this link will get the funds_)
 
-This link will expire in *{{ expiryTime }}*
+This OTP link will expire in *{{ expiryTime }}*
 `
 }
